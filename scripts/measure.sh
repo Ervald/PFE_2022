1f5666e8aa1da20987314c90278234973613684d
Alexandre CABANE <76133063+Ervald@users.noreply.github.com>
2022-02-07 08:37:59 +0100

    Update measure.sh

--- scripts/measure.sh
+++ scripts/measure.sh
@@ -3,5 +3,22 @@
 export ANTSPATH=/home/acabane/Documents/SOFTWARE/ANTs/install/bin
 export PATH=${ANTSPATH}:$PATH
 
-LabelOverlapMeasures 3 ../derivatives/fusion/sub-001/malf_atlas60Labels.nii.gz ../derivatives/atlases/sub-001/ses-1/sub-001_ses-1_7TAMI_DGN.nii.gz 
+M=60 #Nombre de segmentations effectuée
+N=60 #Indice de la segmentation malf
+QuickSeg=true
 
+if QuickSeg=true #type de segmentation
+then
+  o=_QUICK
+  speed=Quick
+else
+  o=''
+  speed=Slow
+fi
+
+cd ../ #Si on place le script dans le dossier script plutôt qu'à la racine de 7TAMIbrain
+
+for i in $(seq -f "%03g" 5 $M)
+do 
+  LabelOverlapMeasures 3 ./derivatives/fusion/sub-${i}/malf_atlas${N}Labels.nii.gz ../derivatives/atlases/sub-${i}/ses-1/sub-${i}_ses-1_7TAMI_DGN.nii.gz /metrics/sub-${i}/sub-${i}_metrics_AtlasTo${speed}
+done
